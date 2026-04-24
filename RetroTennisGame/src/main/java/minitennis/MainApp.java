feb2db3acebe2cc998b8c930ba3309332b6b1759
candelacabell0 <ccabelloromero2122@insgabrielamistral.cat>
2026-04-24 09:43:47 +0200

    Canvis i noves classes: Creació de meú i de control d'idiomes

---
+++ RetroTennisGame/src/main/java/minitennis/MainApp.java
@@ -0,0 +1,13 @@
+package minitennis;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		InitialWindow menu = new InitialWindow();
+
+		// Cridem al mètode que gestiona la tria d'idioma, nom, nivell i regles
+		menu.mostrarMenu();
+	}
+
+}
