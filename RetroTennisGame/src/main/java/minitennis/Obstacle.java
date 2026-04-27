635260b6f261d5335f306c08d9400fc1d2762b2f
Usuario <Usuario@HP>
2026-04-27 09:15:07 +0200

    Cambiado los obstaculos por fantasmas

--- RetroTennisGame/src/main/java/minitennis/Obstacle.java
+++ RetroTennisGame/src/main/java/minitennis/Obstacle.java
@@ -8,7 +8,8 @@ public class Obstacle {
 
     private int x, y;
     private int dx = 2;
-    private static final int SIZE = 20;
+    private static final int WIDTH = 26;
+    private static final int HEIGHT = 28;
 
     private Game game;
 
@@ -25,18 +26,40 @@ public class Obstacle {
             x += dx;
 
             // rebot parets
-            if (x <= 0 || x >= width - SIZE) {
+            if (x <= 0 || x >= width - WIDTH) {
                 dx = -dx;
             }
         }
     }
 
     public void paint(Graphics2D g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(x, y, SIZE, SIZE);
+
+        // cuerpo fantasma
+        g.setColor(new Color(180, 0, 255));
+
+        // cabeza
+        g.fillOval(x, y, WIDTH, 18);
+
+        // cuerpo rectangular
+        g.fillRect(x, y + 9, WIDTH, 12);
+
+        // ondas inferiores
+        g.fillOval(x, y + 16, 8, 12);
+        g.fillOval(x + 9, y + 16, 8, 12);
+        g.fillOval(x + 18, y + 16, 8, 12);
+
+        // ojos
+        g.setColor(Color.WHITE);
+        g.fillOval(x + 6, y + 7, 5, 7);
+        g.fillOval(x + 15, y + 7, 5, 7);
+
+        // pupilas
+        g.setColor(Color.BLACK);
+        g.fillOval(x + 8, y + 10, 2, 3);
+        g.fillOval(x + 17, y + 10, 2, 3);
     }
 
     public Rectangle getBounds() {
-        return new Rectangle(x, y, SIZE, SIZE);
+        return new Rectangle(x, y, WIDTH, HEIGHT);
     }
 }
