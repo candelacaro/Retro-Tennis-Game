0acc2d38b6c5720fbb11b6a7e2e4d8a2ab5fcf20
Usuario <Usuario@HP>
2026-04-28 13:18:06 +0200

    FOTO FANTASMA

--- RetroTennisGame/src/main/java/minitennis/Obstacle.java
+++ RetroTennisGame/src/main/java/minitennis/Obstacle.java
@@ -1,56 +1,86 @@
 package minitennis;
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-
+import java.net.URL;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 /**
- * * @author André Medinas, Candela Cabello, Daner Coria, Izan Perez i Adrià Chenovart
- */
+* * @author André Medinas, Candela Cabello, Daner Coria, Izan Perez i Adrià Chenovart
+*/
 public class Obstacle {
-    private int x; 
-    private int y;
-    private int dx = 2;
-    private static final int WIDTH = 26;
-    private static final int HEIGHT = 28;
-    private Game game;
-    private int vida = 1;
-
-    public Obstacle(int x, int y, Game game, int nivel) {
-        this.x = x;
-        this.y = y;
-        this.game = game;
-        if (nivel >= 10) this.vida = 2;
-    }
-
-    public void move() {
-        if (game.getWidth() > 0) {
-            x += dx;
-            if (x <= 0 || x >= game.getWidth() - WIDTH) dx *= -1;
-        }
-    }
-
-    public void ferDany() { vida--; }
-    public int getVida() { return vida; }
-
-    public void paint(Graphics2D g) {
-        if (vida == 2) g.setColor(Color.YELLOW);
-        else g.setColor(new Color(180, 0, 255));
-
-        g.fillOval(x, y, WIDTH, 18);
-        g.fillRect(x, y + 9, WIDTH, 12);
-        g.fillOval(x, y + 16, 8, 12);
-        g.fillOval(x + 9, y + 16, 8, 12);
-        g.fillOval(x + 18, y + 16, 8, 12);
-        g.setColor(Color.WHITE);
-        g.fillOval(x + 6, y + 7, 5, 7);
-        g.fillOval(x + 15, y + 7, 5, 7);
-        g.setColor(Color.BLACK);
-        g.fillOval(x + 8, y + 10, 2, 3);
-        g.fillOval(x + 17, y + 10, 2, 3);
-    }
-
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, WIDTH, HEIGHT);
-    }
+   private int x;
+   private int y;
+   private int dx = 2;
+   private static final int WIDTH = 26;
+   private static final int HEIGHT = 28;
+ 
+   private static final int IMG_WIDTH = 30;
+   private static final int IMG_HEIGHT = 30;
+  
+   private Game game;
+   private int vida = 1;
+  
+   //atributs imatges
+   private Image imgNormal;
+   private Image imgDanyat;
+  
+   public Obstacle(int x, int y, Game game, int nivel) {
+       this.x = x;
+       this.y = y;
+       this.game = game;
+       try {
+           // Intenta obtenir la URL del fitxer dins del paquet
+           URL urlNormal = getClass().getResource("fantasmaAmarillo.png");
+           URL urlDanyat = getClass().getResource("fantasmaRojo.png");
+          
+           if (urlNormal != null) {
+               imgNormal = new ImageIcon(urlNormal).getImage();
+           } else {
+               System.out.println("Error: No s'ha trobat l'arxiu fantasma_lila.png al paquet minitennis");
+           }
+          
+           if (urlDanyat != null) {
+               imgDanyat = new ImageIcon(urlDanyat).getImage();
+           } else {
+               System.out.println("Error: No s'ha trobat l'arxiu fantasma_blau.png al paquet minitennis");
+           }
+          
+       } catch (Exception e) {
+           System.out.println("Excepció carregant imatges d'obstacles: " + e.getMessage());
+       }
+       if (nivel >= 10) this.vida = 2;
+   }
+   public void move() {
+       if (game.getWidth() > 0) {
+           x += dx;
+           if (x <= 0 || x >= game.getWidth() - WIDTH) dx *= -1;
+       }
+   }
+   public void ferDany() { vida--; }
+   public int getVida() { return vida; }
+   public void paint(Graphics2D g) {
+   	Image imgActual = (vida == 2) ? imgNormal : imgDanyat;
+   	
+   	int offsetDrawX = x - (IMG_WIDTH - WIDTH) / 2;
+       int offsetDrawY = y - (IMG_HEIGHT - HEIGHT) / 2;
+      
+       if (imgActual != null) {
+           g.drawImage(imgActual, offsetDrawX, offsetDrawY, IMG_WIDTH, IMG_HEIGHT, null);
+       } else {
+           // Dibuix d'emergència si la foto falla ---
+           // Així no sortiran invisibles si no troba l'arxiu
+           g.setColor(vida == 2 ? Color.YELLOW : new Color(180, 0, 255));
+           g.fillOval(x, y, WIDTH, HEIGHT);
+           g.setColor(Color.WHITE);
+           g.fillOval(x + 5, y + 5, 5, 5);
+           g.fillOval(x + 15, y + 5, 5, 5);
+       }
+   }
+  
+   
+   public Rectangle getBounds() {
+       return new Rectangle(x, y, WIDTH, HEIGHT);
+   }
 }
+
